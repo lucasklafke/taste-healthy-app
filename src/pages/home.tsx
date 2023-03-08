20015bd8c780618f3ebf208a3750874c208364d3
lucasnkz11 <lucasnkz11@gmail.com>
2023-03-08 15:04:18 -0300

    feat: layout home done

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,18 @@
 import styles from '@/styles/home.module.css'
-
+import Image from 'next/image'
+import homeImage from '@/assets/images/homeImage.png'
 export default function Home() {
   return (
-   <div className={styles.home}>home</div>
+    <div className={styles.home}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Taste Healthy</h1>
+      </div>
+        <Image src={homeImage} alt="" className={styles.image}/>
+        <div className={styles.footer}>
+          <button className={styles.button}><span className={styles.span}>Entrar</span></button>
+          <span className={styles.subtitle}>Ainda não cadastrado? Registre-se!</span>
+        </div>
+    </div>
    
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import styles from '@/styles/splash.module.css'
 import { useEffect } from 'react';
+
 export default function Splash() {
   const router = useRouter()
   useEffect(() => {
@@ -9,8 +10,8 @@ export default function Splash() {
   }, 1000);}, [])
   
   return (
-    <div className={styles.splash}>
-      Taste Healthy
+    <div className={styles.home}>
+      <h1 className={styles.title}>Taste Healthy</h1>
     </div>
   )
 }
