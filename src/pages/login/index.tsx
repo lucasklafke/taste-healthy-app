fd4a38d21879cb5662b323be189cf251d0e89e0b
lucasnkz11 <lucasnkz11@gmail.com>
2023-03-15 15:27:55 -0300

    feat: login formulary

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,32 +2,38 @@ import Image from 'next/image'
 import Header from '@/components/Header'
 import LoginImage from '@/assets/images/LoginImage.png'
 import styles from '@/styles/login.module.css'
+import { useState } from 'react'
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [remeberUser, setRemeberUser] = useState(false)
+  function handleSubmit(e:any) {
+    e.preventDefault()
+    console.log(email, password, remeberUser)
+  }
   return (
   <div className={styles.loginPage}>
     <Header/>
     <Image src={LoginImage} alt="" className={styles.image}/>
 
     <div className={styles.inputContainer}>
-      <div>
-        <span className={styles.label}>E-mail</span>
-        <form autoComplete='off'>
-          <input className={styles.input} placeholder='Digite seu e-mail' type='email' autoComplete='off' />
-        </form>
-      </div>
-      <div>
-      <span className={styles.label}>Password</span>
-      <form autoComplete='off'>
-        <input className={styles.input} placeholder='Digite sua senha' type='password' autoComplete='off'/>
+      <form autoComplete='off' onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label className={styles.label}>E-mail</label>
+            <input className={styles.input} placeholder='Digite seu e-mail' type='email' autoComplete='off' value={email} onChange={(e) => setEmail(e.target.value)}/>
+          </div>
+          <div>
+            <label className={styles.label}>Password</label>
+            <input className={styles.input} placeholder='Digite sua senha' type='password' autoComplete='off' value={password} onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <div className={styles.confirmContainer}>
+            <button className={styles.confirmButton}>Entrar</button>
+            <div className={styles.checkBoxContainer}>
+              <input type='checkbox' onClick={() => setRemeberUser(!remeberUser)}></input>
+              <span className={styles.span}>Lembrar meu usuário</span>
+            </div>
+          </div>
       </form>
-      </div>
-    </div>
-    <div className={styles.confirmContainer}>
-      <button className={styles.confirmButton}>Entrar</button>
-      <div className={styles.checkBoxContainer}>
-        <input type='checkbox'></input>
-        <span className={styles.span}>Lembrar meu usuário</span>
-      </div>
     </div>
   </div>
 
