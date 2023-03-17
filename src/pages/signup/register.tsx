a92225c54b00c33af4991827631759434363a6ec
lucasnkz11 <lucasnkz11@gmail.com>
2023-03-17 13:13:31 -0300

    feat: register formulary done

--- src/pages/signup/register.tsx
+++ src/pages/signup/register.tsx
@@ -1,14 +1,18 @@
 import Header from '@/components/Header';
 import styles from '@/styles/signup.module.css';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Login() {
   const router = useRouter();
-
+  const [username ,setUsername] = useState('')
+  const [gender, setGender] = useState('')
   function handleSubmit(event: any, path: string) {
     event.preventDefault();
+    console.log(username, gender)
     router.push(path);
   }
+  console.log(gender)
   return (
     <div className={styles.page}>
       <Header />
@@ -17,15 +21,15 @@ export default function Login() {
       <form onSubmit={(event) => handleSubmit(event, '/home')} className={styles.form}>
         <div className={styles.inputContainer}>
           <label htmlFor="username">Nome de Usuário</label>
-          <input type="text" name="username" className={styles.input} />
+          <input type="text" name="username" className={styles.input} value={username} onChange={(event) => {setUsername(event.target.value)}}/>
         </div>
         <div className={styles.inputContainer}>
           <label htmlFor="gender">Gênero</label>
           <select name="" id="" className={styles.select}>
-            <option value=""></option>
-            <option value="masculino">masculino</option>
-            <option value="feminino">feminino</option>
-            <option value="outro">outro</option>
+            <option value={gender}></option>
+            <option value="masculino" onClick={() => setGender('masculino')}>masculino</option>
+            <option value="feminino" onClick={() => setGender('feminino')}>feminino</option>
+            <option value="outro" onClick={() => setGender('outro')}>outro</option>
           </select>
         </div>
         <button className={styles.registerButton}>Registrar-se</button>
